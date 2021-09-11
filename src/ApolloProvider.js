8df854821ebe6fdc515d8d6a4ca80632225638bd
amr elgendy <amr86elgendy@gmail.com>
2021-09-11 09:09:24 +0200

    fix

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -29,7 +29,7 @@ const authLink = setContext((_, { headers }) => {
 httpLink = authLink.concat(httpLink)
 const host = window.location.host
 const wsLink = new WebSocketLink({
-  uri: `ws://${host}/graphql`,
+  uri: `wss://${host}/graphql`,
   options: {
     reconnect: true,
     connectionParams: {
